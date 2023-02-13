f850455e08a11eb956cba05fc2725771dc2f39b2
Jkokol2000 <112048803+Jkokol2000@users.noreply.github.com>
2023-02-13 11:45:55 -0800

    Added stat allocation to the character model

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,5 +1,4 @@
 const Character = require('../models/characters')
-const user = require('../models/user')
 const User = require('../models/user')
 
 module.exports = {
@@ -20,27 +19,47 @@ function index(req, res) {
    }})
 }
 
-function newCharacter(req,res) {
-    if (req.user) {
-        User.findOne({ googleId: req.user.googleId}, (err, user) => {
-            if (err) {
-                return res.status(500).send({ message: 'Error finding user'});
-            }
-            const character = new Character({
-                name: req.body.name,
-                user: user._id
-            });
-            character.save((err) => {
-                if (err) {
-                    return res.status(500).send({ message: 'Error Saving character'});
-                }
-                res.redirect('/characters')
-            });
-        });
-    } else {
-        res.status(401).send({message: 'Not authorized'});
+async function newCharacter(req, res) {
+    try {
+    const user = await User.findOne({ googleId: req.user.googleId });
+    // Check if there are any duplicate stats
+    const primaryStat = req.body.primaryStat;
+    const secondaryStat = req.body.secondaryStat;
+    const tertiaryStat = req.body.tertiaryStat;
+    
+    if (primaryStat === secondaryStat || secondaryStat === tertiaryStat || tertiaryStat === primaryStat) {
+      return res.status(400).send({ message: 'Error: Primary, Secondary, and Tertiary stats cannot be the same' });
     }
-}
+    
+    const characterStats = {
+      lifePoints: 0,
+      agility: 2,
+      force: 2,
+      tenacity: 2,
+      scholarship: 2,
+      insight: 2,
+      spirit: 2,
+      lifePoints: 2,
+    };
+    characterStats[primaryStat] = 8;
+    characterStats[secondaryStat] = 6;
+    characterStats[tertiaryStat] = 4;
+    characterStats['lifePoints'] = rollDice(5, 10, characterStats['tenacity']);
+    
+    const character = new Character({
+      name: req.body.name,
+      user: user._id,
+      stats: characterStats,
+    });
+    await character.save();
+    res.redirect('/characters');
+    } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'An error occurred while saving the character' });
+    }
+    }
+    
+    
 function deleteCharacter(req, res) {
     Character.findByIdAndDelete(req.params.id, (err) => {
         if (err) {
@@ -71,4 +90,12 @@ function show(req,res) {
             });
         }
 })
+}
+
+function rollDice(numDice, sides, bonus) {
+    let result = 0;
+    for (let i = 0; i < numDice; i++) {
+        result += Math.floor(Math.random()*sides)+1;
+    }
+    return result + bonus
 }
--- models/characters.js
+++ models/characters.js
@@ -7,6 +7,40 @@ const characterSchema = new Schema ({
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true
+    },
+    stats: {
+        lifePoints: {
+            type: Number,
+            default: 0
+        },
+        agility: {
+            type: Number,
+            default: 2
+        },
+        force: {
+            type: Number,
+            default: 2
+        },
+        tenacity: {
+            type: Number,
+            default: 2
+        },
+        scholarship: {
+            type: Number,
+            default: 2
+        },
+        insight: {
+            type: Number,
+            default: 2
+        },
+        spirit: {
+            type: Number,
+            default: 2
+        },
+        personality: {
+            type: Number,
+            default: 2
+        }
     }
 });
 

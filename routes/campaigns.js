e151141abda4dabb985b3530833175a389c6d8d5
Jkokol2000 <112048803+Jkokol2000@users.noreply.github.com>
2023-02-14 16:11:58 -0800

    Added authorization requiring a user to be logged in order to access Characters or Campaigns. Also improved styling.

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const campaignCtrl = require('../controllers/campaigns');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-router.get('/campaigns', campaignCtrl.index)
+router.get('/campaigns', ensureLoggedIn, campaignCtrl.index)
 router.get('/campaigns/new', campaignCtrl.create)
 router.post('/campaigns', campaignCtrl.new)
 router.get('/campaigns/:id', campaignCtrl.show)
--- routes/characters.js
+++ routes/characters.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const charactersCtrl = require('../controllers/characters');
-
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 // GET /characters (display all characters)
-router.get('/characters', charactersCtrl.index)
+router.get('/characters', ensureLoggedIn, charactersCtrl.index)
 router.get('/characters/new', charactersCtrl.create)
 router.post('/characters', charactersCtrl.new)
 router.get('/characters/:id', charactersCtrl.show)
